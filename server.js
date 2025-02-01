43b9e0826a83ed9f21cd8a774160da528ab58ba1
Saifalikhan99 <myname14saif@gmail.com>
2025-02-01 22:54:24 +0530

    fix url

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {router} from "./src/routes.js";
 import cors from "cors";
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use(cors({ origin: process.env.Frontend_Url }));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the Express API!");
